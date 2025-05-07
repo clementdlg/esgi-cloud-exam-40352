1cd2f089a4416d7b8f015f2cd54adad1eab70999
Clément DE LA GENIERE <0xkrem@proton.me>
2025-05-07 14:57:22 +0200

    added env variables for db connect

--- db.client.js
+++ db.client.js
@@ -1,23 +1,32 @@
-const { Sequelize } = require('sequelize')
+// require('dotenv').config();
 
-// database
-const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
-  {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  },
-);
+const { Sequelize } = require('sequelize');
+
+const {
+	DB_USER,
+	DB_PASSWORD,
+	DB_HOST,
+	DB_PORT,
+	DB_NAME,
+} = process.env;
+
+const databaseUrl = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
+const sequelize = new Sequelize(databaseUrl, {
+	dialect: 'postgres',
+	dialectOptions: {
+		ssl: {
+			require: true,
+			rejectUnauthorized: false,
+		},
+	},
+});
 
 // authentication and synchronization
 sequelize.authenticate()
-  .then(() => {
-    sequelize.sync().catch(() => console.log("Cannot sync the database"));
-  })
-  .catch(() => console.log("Cannot connect to database, please check environment credentials"));
+	.then(() => {
+		sequelize.sync().catch(() => console.log("Cannot sync the database"));
+	})
+	.catch(() => console.log("Cannot connect to database, please check environment credentials"));
 
 module.exports = sequelize;
